b7a46f53cbfd56aedb331e41ee5204ebcac1bac1
Shane Phillips <shane@loongphillips.com>
2023-12-27 15:31:25 -0500

    New Card button, cards array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 2;
 let secondCard = 10;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive = true;
@@ -10,18 +11,31 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 const startGame = () => {
+	renderGame();
+};
+
+const renderGame = () => {
 	// sumEl.textContent += sum;
 	sumEl.textContent = "Sum: " + sum;
 	cardsEl.textContent =
-		"Cards: " + firstCard + " " + secondCard;
+		"Cards: " + cards[0] + " " + cards[1];
 	if (sum <= 20) {
 		message = "Draw a new card?";
 	} else if (sum === 21) {
-		message = "Gratz!";
+		message = "You got Blackjack! Congrats!";
 		hasBlackjack = true;
 	} else {
 		message = "You lose!";
 		isAlive = false;
 	}
 	messageEl.textContent = message;
+};
+
+const newCard = () => {
+	// console.log("Drawing new card!");
+	let card = 9;
+	sum += card;
+	cards.push(card);
+	console.log(cards);
+	renderGame();
 };
