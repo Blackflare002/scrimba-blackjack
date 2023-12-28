399d8d02242a398b01135bdd1241a179b96137ee
Shane Phillips <shane@loongphillips.com>
2023-12-28 15:34:54 -0500

    Random number function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 2;
-let secondCard = 10;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
@@ -10,15 +10,28 @@ let messageEl =
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+function getRandomCard() {
+	let theNumber =
+		Math.floor(Math.random() * 13) + 1;
+	if (theNumber === 1) {
+		return 11;
+	} else if (theNumber > 10) {
+		return 10;
+	} else {
+		return theNumber;
+	}
+} //hoisted!
+
 const startGame = () => {
 	renderGame();
 };
 
 const renderGame = () => {
-	// sumEl.textContent += sum;
+	cardsEl.textContent = "Cards: ";
+	for (let i = 0; i < cards.length; i++) {
+		cardsEl.textContent += cards[i] + " ";
+	}
 	sumEl.textContent = "Sum: " + sum;
-	cardsEl.textContent =
-		"Cards: " + cards[0] + " " + cards[1];
 	if (sum <= 20) {
 		message = "Draw a new card?";
 	} else if (sum === 21) {
@@ -33,7 +46,7 @@ const renderGame = () => {
 
 const newCard = () => {
 	// console.log("Drawing new card!");
-	let card = 9;
+	let card = getRandomCard();
 	sum += card;
 	cards.push(card);
 	console.log(cards);
